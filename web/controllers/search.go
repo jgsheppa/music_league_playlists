111db684f565e3fb7711a621cc4b764033d29e7d
jgsheppa <jgsheppa@protonmail.com>
2024-04-17 01:21:55 +0200

    create basic ui for search

---
+++ web/controllers/search.go
@@ -0,0 +1,75 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/jgsheppa/music_league_playlists/internal/search"
+	"github.com/labstack/echo/v4"
+)
+
+func NewSearch(sc *search.ElasticSearch) *Search {
+	return &Search{
+		sc: sc,
+	}
+}
+
+type Search struct {
+	sc *search.ElasticSearch
+}
+
+func (s *Search) Result(c echo.Context) error {
+	name := c.QueryParam("name")
+
+	s.sc.WithIndex(search.TrackIndex)
+	s.sc.WithQuery(name)
+	res, err := s.sc.SearchField()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("could not decode json: %e", err)
+	}
+
+	var foundPlaylists search.TrackSearchResponse
+	err = json.Unmarshal(body, &foundPlaylists)
+	if err != nil {
+		log.Fatalf("could not unmarshall json: %e", err)
+	}
+
+	return c.Render(http.StatusOK, "track", foundPlaylists.Hits)
+}
+
+func (s *Search) Home(c echo.Context) error {
+	query := c.QueryParam("query")
+	var foundPlaylists search.TrackSearchResponse
+	fmt.Println(foundPlaylists)
+
+	if len(query) > 0 {
+		s.sc.WithIndex(search.TrackIndex)
+		s.sc.WithQuery(query)
+		res, err := s.sc.SearchField()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(res)
+
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			log.Fatalf("could not decode json: %e", err)
+		}
+
+		err = json.Unmarshal(body, &foundPlaylists)
+		if err != nil {
+			log.Fatalf("could not unmarshall json: %e", err)
+		}
+	}
+
+	return c.Render(http.StatusOK, "home", foundPlaylists)
+}
--- web/main.go
+++ web/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"text/template"
 
 	"github.com/jgsheppa/music_league_playlists/internal/search"
+	"github.com/jgsheppa/music_league_playlists/web/controllers"
+	"github.com/jgsheppa/music_league_playlists/web/views"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
 )
@@ -20,43 +17,12 @@ func main() {
 	}
 	client := search.NewSearchClient(esClient)
 
-	t := &Template{
-		templates: template.Must(template.ParseGlob("web/views/*.html")),
-	}
+	t := views.NewTemplate()
+
+	searchController := controllers.NewSearch(client)
 
 	e := echo.New()
 	e.Renderer = t
-	e.GET("/tracks", func(c echo.Context) error {
-		name := c.QueryParam("name")
-
-		client.WithIndex(search.TrackIndex)
-		client.WithQuery(name)
-		res, err := client.SearchField()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(res)
-
-		body, err := io.ReadAll(res.Body)
-		if err != nil {
-			log.Fatalf("could not decode json: %e", err)
-		}
-
-		var foundPlaylists search.TrackSearchResponse
-		err = json.Unmarshal(body, &foundPlaylists)
-		if err != nil {
-			log.Fatalf("could not unmarshall json: %e", err)
-		}
-
-		return c.Render(http.StatusOK, "track", foundPlaylists.Hits)
-	})
+	e.GET("/", searchController.Home)
 	e.Logger.Fatal(e.Start(":8080"))
-}
-
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
 }
