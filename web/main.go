891e81289c0b7f10e4b5bd90d8bc2da34bf3b652
jgsheppa <jgsheppa@protonmail.com>
2024-04-18 01:34:52 +0200

    include all assets in repo

--- web/main.go
+++ web/main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"log"
 
-	"github.com/jgsheppa/music_league_playlists/internal/playlists"
 	"github.com/jgsheppa/music_league_playlists/internal/search"
-	"github.com/jgsheppa/music_league_playlists/internal/tracks"
 	"github.com/jgsheppa/music_league_playlists/web/controllers"
 	"github.com/jgsheppa/music_league_playlists/web/views"
 	_ "github.com/joho/godotenv/autoload"
@@ -13,20 +11,12 @@ import (
 )
 
 func init() {
-	if err := playlists.Run(); err != nil {
-		log.Fatalf("could not run playlist script to completion: %e", err)
-	}
-
 	if err := search.RunIndexPlaylists(); err != nil {
-		log.Fatalf("could not run search script: %e", err)
-	}
-
-	if err := tracks.Run(); err != nil {
-		log.Fatalf("could not run track command: %e", err)
+		log.Fatalf("could not index playlists: %e", err)
 	}
 
 	if err := search.RunIndexTracks(); err != nil {
-		log.Fatalf("could not run search script: %e", err)
+		log.Fatalf("could not index tracks: %e", err)
 	}
 }
 func main() {
