3db55c28720eb4321bc43c34fcaa69068fbbf9dc
jgsheppa <jgsheppa@protonmail.com>
2024-04-18 01:23:24 +0200

    add init func

--- web/main.go
+++ web/main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"log"
 
+	"github.com/jgsheppa/music_league_playlists/internal/playlists"
 	"github.com/jgsheppa/music_league_playlists/internal/search"
+	"github.com/jgsheppa/music_league_playlists/internal/tracks"
 	"github.com/jgsheppa/music_league_playlists/web/controllers"
 	"github.com/jgsheppa/music_league_playlists/web/views"
 	_ "github.com/joho/godotenv/autoload"
 	"github.com/labstack/echo/v4"
 )
 
+func init() {
+	if err := playlists.Run(); err != nil {
+		log.Fatalf("could not run playlist script to completion: %e", err)
+	}
+
+	if err := search.RunIndexPlaylists(); err != nil {
+		log.Fatalf("could not run search script: %e", err)
+	}
+
+	if err := tracks.Run(); err != nil {
+		log.Fatalf("could not run track command: %e", err)
+	}
+
+	if err := search.RunIndexTracks(); err != nil {
+		log.Fatalf("could not run search script: %e", err)
+	}
+}
 func main() {
 	esClient, err := search.SetupElasticClient()
 	if err != nil {
