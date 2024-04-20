3d0421cf28f8f33b1932331e6c449bb173c86c5d
jgsheppa <jgsheppa@protonmail.com>
2024-04-20 23:07:39 +0200

    make elastic production connection work

--- go.mod
+++ go.mod
@@ -7,13 +7,13 @@ require (
 	github.com/dustin/go-humanize v1.0.1
 	github.com/elastic/go-elasticsearch/v8 v8.13.0
 	github.com/joho/godotenv v1.5.1
+	github.com/labstack/echo/v4 v4.12.0
 )
 
 require (
 	github.com/elastic/elastic-transport-go/v8 v8.5.0 // indirect
 	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
-	github.com/labstack/echo/v4 v4.12.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- internal/search/script.go
+++ internal/search/script.go
@@ -34,16 +34,28 @@ func RunIndexTracks(esClient *elasticsearch.Client) error {
 }
 
 func SetupElasticClient() (*elasticsearch.Client, error) {
-	esURL := os.Getenv("ENV_ES_URL")
+	esURL := os.Getenv("ES_URL")
 	if esURL == "" {
 		return nil, errors.New("elasticsearch url cannot be empty")
+	}
+
+	apiKey := os.Getenv("ESC_API_KEY")
+	if esURL == "" {
+		return nil, errors.New("elasticsearch api key cannot be empty")
+	}
+
+	cloudID := os.Getenv("ES_CLOUD_ID")
+	if esURL == "" {
+		return nil, errors.New("elasticsearch cloud id cannot be empty")
 	}
 
 	// Use a third-party package for implementing the backoff function
 	retryBackoff := backoff.NewExponentialBackOff()
 
 	esConfig := elasticsearch.Config{
-		Addresses:     []string{esURL},
+		// Addresses:     []string{esURL},
+		CloudID:       cloudID,
+		APIKey:        apiKey,
 		RetryOnStatus: []int{502, 503, 504, 429},
 		RetryBackoff: func(i int) time.Duration {
 			if i == 1 {
